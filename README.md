a34add084fa20b988e55336c94f1b72864f40a4e
bierdok <2333728+bierdok@users.noreply.github.com>
2025-02-23 08:21:51 +0100

    Migrate to GitHub

---
+++ README.md
@@ -0,0 +1,56 @@
+# TalkOps Addon: Picovoice Porcupine
+
+A TalkOps Addon made to work with [TalkOps](https://link.talkops.app/talkops).
+
+[Picovoice Porcupine](https://picovoice.ai/platform/porcupine/) is a lightweight, highly accurate, and customizable wake word detection engine. It allows devices to respond to specific trigger words (wake words) without needing a continuous internet connection. It&#39;s designed to run efficiently on embedded devices like microcontrollers and mobile devices.
+
+## Installation Guide
+
+Install [TalkOps](https://link.talkops.app/install-talkops).
+
+
+## Integration Guide
+
+Add the service and setup the environment variables if needed:
+
+_compose.yml_
+```
+name: talkops
+
+services:
+...
+  talkops-addon-picovoice-porcupine:
+    build:
+      context: [dockerfile-path]
+    environment:
+      ACCESS_KEY: [your-value]
+    volumes:
+      - ./keywords:/data
+    restart: unless-stopped
+```
+
+## Environment Variables
+
+#### ACCESS_KEY
+
+The access key provided by Picovoice. Sign up at [Picovoice Console](https://console.picovoice.ai/signup) to obtain your access key.
+
+#### LANGUAGE
+
+The language of keywords.
+* Default value: `English`
+* Available values: `Arabic` `Chinese` `Dutch` `English` `French` `German` `Hindi` `Italian` `Japanese` `Korean` `Persian` `Polish` `Portuguese` `Russian` `Spanish` `Swedish` `Vietnamese`
+
+#### SENSITIVITY
+
+The sensitivity between 0 and 1. A higher sensitivity reduces miss rate at cost of increased false alarm rate.
+* Default value: `0.6`
+
+#### AGENT_URLS
+
+A comma-separated list of WebSocket server URLs for real-time communication with specified agents.
+* Default value: `ws://talkops`
+
+## Join the community
+
+* [Discord](https://link.talkops.app/discord)
---
+++ index.js
@@ -0,0 +1,122 @@
+import { Addon, Service, Readme } from "talkops";
+import AdmZip from "adm-zip";
+import fs from "fs";
+import path from "path";
+
+import languages from "./parameters/languages.json" assert { type: "json" };
+
+const addon = new Addon("Picovoice Porcupine");
+
+kernel.setDockerHubRepository("talkops/talkops-addon-picovoice-porcupine");
+
+addon.setDockerVolumeData("keywords");
+
+addon.setEnvironmentVariables({
+  ACCESS_KEY: {
+    description:
+      "The access key provided by Picovoice. Sign up at [Picovoice Console](https://console.picovoice.ai/signup) to obtain your access key.",
+  },
+  LANGUAGE: {
+    defaultValue: "English",
+    description: "The language of keywords.",
+    availableValues: Object.values(languages),
+  },
+  SENSITIVITY: {
+    description:
+      "The sensitivity between 0 and 1. A higher sensitivity reduces miss rate at cost of increased false alarm rate.",
+    defaultValue: 0.6,
+    validation: (v) => parseFloat(v) >= 0 && parseFloat(v) <= 1,
+  },
+});
+
+const accessKey = process.env.ACCESS_KEY;
+const sensitivity = parseFloat(process.env.SENSITIVITY);
+function getLanguageCode(language) {
+  for (let [name, value] of Object.entries(languages)) {
+    if (value === language) {
+      return name;
+    }
+  }
+  return undefined;
+}
+const languageCode = getLanguageCode(process.env.LANGUAGE);
+
+function updateKeywords() {
+  addon.errors = [];
+  const keywords = new Map();
+  try {
+    const fileNames = fs.readdirSync("/data");
+    for (const fileName of fileNames) {
+      if (fileName.startsWith(".")) {
+        continue;
+      }
+      const filePath = path.join("/data", fileName);
+      const fileExtension = path.extname(fileName);
+      const fileNameWithoutExtension = path.basename(
+        fileName,
+        path.extname(fileName)
+      );
+      const fileNameSplitted = fileName.split("_");
+      const fileKeyword = fileNameSplitted[0];
+      const filelanguageCode = fileNameSplitted[1];
+      if (filelanguageCode !== languageCode) {
+        console.error(`Invalid language: ${fileName}`);
+        continue;
+      }
+      const filePlatform = fileNameSplitted[2];
+      if (filePlatform !== "wasm") {
+        console.error(`Invalid platform: ${fileName}`);
+        continue;
+      }
+
+      let content = null;
+      if (fileExtension === ".zip") {
+        try {
+          const zip = new AdmZip(filePath);
+          const entries = zip.getEntries();
+          const ppnEntry = entries.find((entry) =>
+            entry.entryName.endsWith(".ppn")
+          );
+          if (ppnEntry) {
+            content = ppnEntry.getData();
+          }
+        } catch (err) {
+          console.error(err);
+        }
+      } else if (fileExtension === ".ppn") {
+        content = fs.readFileSync(filePath);
+      } else {
+        console.error(`Invalid extension: ${fileName}`);
+        continue;
+      }
+      if (fileKeyword && content) {
+        keywords.set(fileName, {
+          label: fileKeyword,
+          base64: content.toString("base64"),
+          sensitivity,
+          customWritePath: fileNameWithoutExtension,
+        });
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+  if (keywords.size === 0) {
+    addon.errors.push("At least one keyword must be added to the data volume.");
+    process.exit(1);
+  }
+
+  addon.setParameters({
+    accessKey,
+    languageCode,
+    keywords: [...keywords.values()],
+  });
+}
+if (addon.errors.length === 0) {
+  updateKeywords();
+  fs.watch("/data", updateKeywords);
+}
+
+new Readme(process.env.README_TEMPLATE_URL, "/app/README.md", addon);
+new Service(process.env.AGENT_URLS.split(","), addon);
